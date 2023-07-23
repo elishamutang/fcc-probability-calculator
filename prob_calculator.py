cc1fb16b54f50472ced5cc580ef54478587df99d
elishamutang <elishamutang@outlook.com>
2023-07-23 17:49:32 +0800

    Worked on draw method

--- prob_calculator.py
+++ prob_calculator.py
@@ -3,9 +3,23 @@ import random
 
 class Hat:
 
-    pass
+    def __init__(self, **kwargs): 
+        self.kwargs = kwargs
+        self.contents = []
+        for k,v in self.kwargs.items():
+            for color in range(0, v):
+                self.contents.append(k)
 
-
+    def draw(self, balls_drawn): #Method used to draw/remove balls at random from contents variable
+        self.balls_drawn = int(balls_drawn) #Should receive an integer
+        if self.balls_drawn < len(self.contents):
+            self.sample_list = random.sample(self.contents, k=balls_drawn)
+            return self.sample_list
+        else:
+            return self.contents
+        
+    def __str__(self):
+        return f"{self.contents}"
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
