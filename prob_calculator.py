79ce73b201cdb96c50ec1e384d17771670dc19e0
elishamutang <elishamutang@outlook.com>
2023-07-26 22:22:37 +0800

    Finally DONE!

--- prob_calculator.py
+++ prob_calculator.py
@@ -25,16 +25,21 @@ class Hat:
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
 
     M = 0 #how many times you get at least two red balls and one green ball
-    N = num_experiments
     balls_expect = Hat(**expected_balls) #Unpack dictionary using ** to pass into Hat class.
 
-    start = copy.deepcopy(hat.contents)
-    experiment_draw = hat.draw(num_balls_drawn)
-    print(experiment_draw)
-
+    for i in range(num_experiments):
+        start = copy.deepcopy(hat)
+        experiment_draw = start.draw(num_balls_drawn)
+        count = 0
+        for m in balls_expect.contents:
+            if m in experiment_draw:
+                experiment_draw.remove(m)
+                count += 1
+            else:
+                pass
+        if count == len(balls_expect.contents):
+            M += 1
     
-    
+    probability = M/num_experiments
 
-    
-    return
-
+    return probability
